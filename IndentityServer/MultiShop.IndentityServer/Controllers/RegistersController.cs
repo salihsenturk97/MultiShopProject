ed7a35db4c43cb61b98fcf725fe0af8683368647
salihsenturk97 <salihsenturk97@gmail.com>
2024-07-14 18:28:10 +0300

    Identity microservice created and configurations added

---
+++ IndentityServer/MultiShop.IndentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IndentityServer.Dtos;
+using MultiShop.IndentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IndentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                Surname = userRegisterDto.Surname,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                UserName = userRegisterDto.UserName,
+            };
+            var result = await _userManager.CreateAsync(values,userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı Başarıyla Eklendi");
+            }
+            else {
+                return Ok("Bir hata oluştu tekrar deneyiniz.");
+            }
+
+        }
+    }
+}
